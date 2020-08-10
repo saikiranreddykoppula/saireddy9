0debd7635e577328fee77ad3189c4aee40444d46
saikiranreddykoppula <saikiranreddykoppula214@gmail.com>
2020-08-10 13:41:50 +0530

    HI SAI

---
+++ Actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -0,0 +1,14 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void runTest()
+	{
+		Reporter.log("success",true);
+	}
+
+}
